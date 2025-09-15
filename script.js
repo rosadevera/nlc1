cd5a3c83cdb5965016cf5df13170907e081de42f
rosadevera <dever014@newschool.edu>
2025-09-15 13:44:09 -0400

    2

--- script.js
+++ script.js
@@ -1,24 +1,67 @@
 $(document).ready(function() {
-  const audio = document.getElementById('myAudio');
-  audio.volume = 0.3;
-  const musicToggle = $('#musicToggle');
 
-  // Check if audio was playing before navigating away
-  const audioState = localStorage.getItem('audioState');
-  if (audioState === 'playing') {
-    audio.play();
-  }
+  $(".landing").on("click", function() {
+    $(".landing").addClass("hidden");
+  });
+
+
+  $(".me").on("click", function() {
+    $("#mebox").toggleClass("bar");
+  });
+  $("#mebox").on("click", function() {
+    $("#mebox").removeClass("bar");
+  });
+
+  $(".laptop").on("click", function() {
+    $("#laptopbox").toggleClass("bar");
+  });
+  $("#laptopbox").on("click", function() {
+    $("#laptopbox").removeClass("bar");
+  });
+
+  $(".door").on("click", function() {
+    $("#doorbox").toggleClass("bar");
+  });
+  $("#doorbox").on("click", function() {
+    $("#doorbox").removeClass("bar");
+  });
+
+  $(".heytea").on("click", function() {
+    $("#heyteabox").toggleClass("bar");
+  });
+  $("#heyteabox").on("click", function() {
+    $("#heyteabox").removeClass("bar");
+  });
+
+  $(".window").on("click", function() {
+    $("#windowbox").toggleClass("bar");
+  });
+  $("#windowbox").on("click", function() {
+    $("#windowbox").removeClass("bar");
+  });
+
+  $(".headphones").on("click", function() {
+    $("#headphonesbox").toggleClass("bar");
+  });
+  $("#headphonesbox").on("click", function() {
+    $("#headphonesbox").removeClass("bar");
+  });
+
+  $(".job").on("click", function() {
+    $("#jobbox").toggleClass("bar");
+  });
+  $("#jobbox").on("click", function() {
+    $("#jobbox").removeClass("bar");
+  });
+
 });
 
+
   $( function() {
-    $(".bambigreenhouse").draggable();
-    $(".pileagreenhouse").draggable();
-    $(".rosagreenhouse").draggable();
-    $(".pothosgreenhouse").draggable();
-    $(".wateringcangreenhouse").draggable();
-    $(".scissorsgreenhouse").draggable();
-    $(".fertilizergreenhouse").draggable();
-    $(".sonnygreenhouse").draggable();
-    $(".pixelgreenhouse").draggable();
-    $(".ecosystemgreenhouse").draggable();
+    $(".room").draggable();
+    $(".me").draggable();
+    $(".laptop").draggable();
+    $(".heytea").draggable();
+    $(".headphones").draggable();
+    $(".job").draggable();
   });
